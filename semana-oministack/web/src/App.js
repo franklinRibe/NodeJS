85a1a5fa5af8ee09ceea8f9edb145bb7f70038b6
Franklin Ribeiro <franklin.ribeiro@scielo.org>
2020-01-16 16:59:42 -0300

    Estilizando app

--- semana-oministack/web/src/App.js
+++ semana-oministack/web/src/App.js
@@ -1,8 +1,46 @@
 import React from 'react';
+// Componente: Bloco isolado de HTML, CSS e JS o qual nao interfere no restante da aplicacao
+// Propriedade: Informações que o componente pai passa para o componente filho
+// Estado: Informaçoes mantidas pelo componente (lembrar: imutabilidade)
+
+import './global.css'
+import './app.css'
+import './sidebar.css'
 
 function App() {
+
   return (
-  <h1>Testando a bagaça</h1>
+    <div id="app">
+      <aside>
+          <strong>Cadastrar</strong>
+          <form>
+            <div className="input-block>">
+                <label htmlFor="github_username">Usuário do Github</label>
+                <input name="github_username" id="github_username" required></input>
+            </div>
+            <div className="input-block>">
+                <label htmlFor="techs">Tecnologias</label>
+                <input name="techs" id="techs" required></input>
+            </div>
+            
+            <div className="input-group">
+              <div className="input-block>">
+                  <label htmlFor="latitude">Latitude</label>
+                  <input name="latitude" id="latitude" required></input>
+              </div>
+              <div className="input-block>">
+                <label htmlFor="longitude">Longitude</label>
+                <input name="longitude" id="longitude" required></input>
+              </div>
+            </div>
+          
+            <button type="submit">Salvar</button>
+          </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
