9847cd6608e5ddda98244e257ab18af1ea73c7f4
Franklin Ribeiro <franklin.ribeiro@scielo.org>
2020-01-16 17:05:57 -0300

    estilizando

--- semana-oministack/web/src/App.js
+++ semana-oministack/web/src/App.js
@@ -14,21 +14,21 @@ function App() {
       <aside>
           <strong>Cadastrar</strong>
           <form>
-            <div className="input-block>">
+            <div className="input-block">
                 <label htmlFor="github_username">Usuário do Github</label>
                 <input name="github_username" id="github_username" required></input>
             </div>
-            <div className="input-block>">
+            <div className="input-block">
                 <label htmlFor="techs">Tecnologias</label>
                 <input name="techs" id="techs" required></input>
             </div>
             
             <div className="input-group">
-              <div className="input-block>">
+              <div className="input-block">
                   <label htmlFor="latitude">Latitude</label>
                   <input name="latitude" id="latitude" required></input>
               </div>
-              <div className="input-block>">
+              <div className="input-block">
                 <label htmlFor="longitude">Longitude</label>
                 <input name="longitude" id="longitude" required></input>
               </div>
